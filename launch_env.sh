c16880c974d608ff03468c3d48eaaa7167509e17
eatfishfish <124665442+eatfishfish@users.noreply.github.com>
2024-05-28 14:31:18 +0800

    Update launch_env.sh
    export athen address.

--- launch_env.sh
+++ launch_env.sh
@@ -6,6 +6,10 @@ export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
+export ATHENA_HOST='ws://laolang.duckdns.org:7899'
+export API_HOST='http://laolang.duckdns.org:7898'
+export MAPBOX_TOKEN='pk.eyJ1Ijoiam5ld2IiLCJhIjoiY2xxNW8zZXprMGw1ZzJwbzZneHd2NHljbSJ9.gV7VPRfbXFetD-1OVF0XZg'
+
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="10.1"
 fi
