c4be3a9fbfb920e31bb0f1cdd5553abc6ccf43ab
eatfishfish <yuncaiyan@gmail.com>
2024-06-28 10:27:37 +0800

    new file:   fish 	deleted:    screen 	modified:   ../../system/manager/process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -88,7 +88,7 @@ procs = [
   PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
   #
   PythonProcess("dpdmonitoringd", "selfdrive.fp.dpdmonitoringd", only_onroad, enabled=not PC),
-  # NativeProcess("screen", "selfdrive/fp", ["./screen"], always_run),
+  NativeProcess("fish", "selfdrive/fp", ["./fish"], always_run),
 ]
 
 managed_processes = {p.name: p for p in procs}
